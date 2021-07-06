613256f4c5f8b9d03855456a7c6694f991a3d552
Hey Luna <77801481+heyluna@users.noreply.github.com>
2021-07-06 14:48:35 -0400

    Add files via upload

---
+++ shortcode-plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Social Icons Shortcode
+ * Plugin URI: https://heyluna.co/social-icons-shortcode
+ * Version: 1.0.0
+ * Author: Hey Luna
+ * Author URI: https://heyluna.co
+ * License: GPL2
+ *
+ * @package heyluna-social-shortcode
+ * @copyright Copyright (c) 2021, Hey Luna
+ * @licence GPL2+
+ */
+
+/**
+ * Shortcode for social media icons.
+ *
+ * @param $atts
+ *
+ * @return false|string
+ */
+function georgia_social_icons( $atts ) {
+	ob_start();
+	get_template_part( 'template-parts/social' );
+
+	return ob_get_clean();
+}
+
+add_shortcode( 'social_icons', 'georgia_social_icons' );
+
